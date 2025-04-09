a76ff8e0e3e123557f3f903e7246d30c83cc8d64
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-04-09 17:12:55 -0500

    feat: update dependencies and add new UI components

---
+++ src/views/Login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/custom/auth/LoginForm";
+
+
+const Login = () => {
+  return <div className="bg-gray-200 h-screen w-full">
+    <LoginForm/>
+  </div>;
+};
+export default Login;
