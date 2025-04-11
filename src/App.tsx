02301e485e7584a3aa7d189543094a160a428ba8
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-04-11 09:21:17 -0500

    configuration for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import { ProtectedRoute } from "./middleware/ProtectedRoute";
