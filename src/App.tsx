352b54eb945aa6f76647a235459121fe433d6e95
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-04-11 09:02:28 -0500

    Add dependencies and update routing and alert configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './views/Home'
-import Login from './views/Login'
-import { ProtectedRoute } from './middleware/ProtectedRoute'
-import Forbidden from './views/Foorbiden'
-import Dashboard from './views/Dashboard'
-
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import { ProtectedRoute } from "./middleware/ProtectedRoute";
+import Forbidden from "./views/Foorbiden";
+import Dashboard from "./views/Dashboard";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={
-          <ProtectedRoute>
-          <Home/>
-          </ProtectedRoute>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path='/forbidden' element={<Forbidden/>}/>
-        <Route path='/dashboard/*' element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forbidden" element={<Forbidden />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-    </BrowserRouter>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
